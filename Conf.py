6b3cd9e24934d2ae856d51444c163b0537342264
Travis <travis@travis-Satellite-P775.(none)>
2013-03-28 00:30:56 -0700

    updated

--- Conf.py
+++ Conf.py
@@ -1,7 +1,7 @@
 class Conf(object):
     def __init__(self):
-        self.secret_access_key = "8a+mG0fFfgJ/atpz+9eveS/jsU+K9iEU2m6EVYuk"
-        self.access_key_id = "AKIAJDAY32NXBCWZMLUQ"
+        self.secret_access_key = "secret key here"
+        self.access_key_id = "access key id here"
         self.endpoint_url = "webservices.amazon.com"
         self.endpoint_path = "/onca/xml"
         
